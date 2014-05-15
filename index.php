894f38d5f4cf3d5c4b344acdaa877ce8ce4b7ed6
neelhtaky <Kat@kathleens-mbp.lan>
2014-05-15 13:04:13 +0300

    Revert df411a4..0b5fa03
    This rolls back to commit df411a4553b75cece68b43881a69d6367d816d4a.

--- index.php
+++ index.php
@@ -3,34 +3,48 @@
 		<!-- MAIN CONTENT WRAPPER -->
 		<div id="main_wrapper" class="small-12 medium-8 large-6 xlarge-6 xxlarge-6 columns" >
 
+			<?php
+				if ( have_posts() ) {
+					while ( have_posts() ) {
+						the_post(); ?>
 
-			    <div class="row">
-			    		<div id="container" class="large-12 columns transitions-enabled large-centered clearfix">
-			               	<div class="box col2"><img src="http://www.placehold.it/225x140" /></div>
-			               	<div class="box col2"><img src="http://www.placehold.it/210x160" /></div>
-			               	<div class="box col2"><img src="http://www.placehold.it/310x280" /></div>
-			               	<div class="box col2"><img src="http://www.placehold.it/200x115" /></div>
-			               	<div class="box col2"><img src="http://www.placehold.it/200x150" /></div>
-			               	<div class="box col2"><img src="http://www.placehold.it/200x130" /></div>
-			               	<div class="box col2"><img src="http://www.placehold.it/200x350" /></div>
-			               	<div class="box col2"><img src="http://www.placehold.it/200x130" /></div>
-			               	<div class="box col2"><img src="http://www.placehold.it/200x130" /></div>
-			               	<div class="box col2"><img src="http://www.placehold.it/200x170" /></div>
-			               	<div class="box col2"><img src="http://www.placehold.it/200x140" /></div>
-			               	<div class="box col2"><img src="http://www.placehold.it/200x110" /></div>
-			               	<div class="box col2"><img src="http://www.placehold.it/200x300" /></div>
-			               	<div class="box col2"><img src="http://www.placehold.it/200x130" /></div>
-			               	<div class="box col2"><img src="http://www.placehold.it/200x180" /></div>
-			               	<div class="box col2"><img src="http://www.placehold.it/300x140" /></div>
-			               	<div class="box col2"><img src="http://www.placehold.it/260x180" /></div>
-			               	<div class="box col2"><img src="http://www.placehold.it/200x135" /></div>
-			               	<div class="box col2"><img src="http://www.placehold.it/240x110" /></div>
-			               	<div class="box col2"><img src="http://www.placehold.it/200x180" /></div>
-			               	<div class="box col2"><img src="http://www.placehold.it/200x150" /></div>
-							<div class="box col2"><img src="http://www.placehold.it/200x120" /></div>
-						</div><!--/container -->
-				    </div>
 
+
+
+						<div class="post xlarge-6 xxlarge-6 columns" data-equalizer-watch>
+
+							<aside class="thumbnail small-12 medium-6 large-6 xlarge-12 xxlarge-12 columns">
+								<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" ><?php the_post_thumbnail(); ?></a>
+							</aside>
+
+						<h2 class="post_title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+
+						<footer class="byline meta postmetadata">
+							<time class="published" datetime="<?php the_time('l, F jS, Y') ?>"><?php the_time('l, F jS, Y') ?></time>.
+							<div class="author">Written by <?php the_author(); ?>.</div>
+							<a href="<?php comments_link(); ?>" class="comments-link">
+								<?php comments_number( 'No Responses Yet. Leave a response?','1 Response.', '% Responses.', 'comments-link', 'Sorry, Comments are closed.'); ?></a>
+								<?php the_category(', ') ?>.
+							<span class="tags"><?php the_tags('Tags: ', ', ', '. '); ?></span>
+						</footer>
+						<p><?php the_excerpt(); ?></p>
+
+						</div>
+
+
+
+
+
+
+
+
+
+
+
+
+					<?php } // end while
+				} // end if
+			?>
 
 		</div><!-- #main_wrapper -->
 
