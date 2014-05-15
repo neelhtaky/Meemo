5e1cb57f6750ebd3a2fb979e818beab529db5fbf
neelhtaky <Kat@kathleens-mbp.lan>
2014-05-15 17:55:26 +0300

    Working Masonry

--- index.php
+++ index.php
@@ -1,52 +1,48 @@
 <?php get_header(); ?>
+<script src="<?php bloginfo('template_directory'); ?>/bower_components/masonry/dist/masonry.pkgd.min.js"></script>
 
 		<!-- MAIN CONTENT WRAPPER -->
-		<div id="main_wrapper" class="small-12 medium-8 large-6 xlarge-6 xxlarge-6 columns" >
+		<div id="wrap" class="small-12 medium-8  large-6 xlarge-7  xxlarge-7 columns ">
 
-			<?php
-				if ( have_posts() ) {
-					while ( have_posts() ) {
-						the_post(); ?>
+			<div id="main_wrapper" class="js-masonry "
+			  data-masonry-options='{
+  "itemSelector": ".post" }' >
 
+				<?php
+					if ( have_posts() ) {
+						while ( have_posts() ) {
+							the_post(); ?>
 
+									<div class="post small-12 medium-12 xlarge-6 xxlarge-6  columns" >
 
+										<aside class="thumbnail small-12 medium-6 large-6 xlarge-12 xxlarge-12 columns">
+											<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" ><?php the_post_thumbnail(); ?></a>
+										</aside>
 
-						<div class="post xlarge-6 xxlarge-6 columns" data-equalizer-watch>
+									<h2 class="post_title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 
-							<aside class="thumbnail small-12 medium-6 large-6 xlarge-12 xxlarge-12 columns">
-								<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" ><?php the_post_thumbnail(); ?></a>
-							</aside>
+									<footer class="byline meta postmetadata">
+										<time class="published" datetime="<?php the_time('l, F jS, Y') ?>"><?php the_time('l, F jS, Y') ?></time>.
+										<div class="author">Written by <?php the_author(); ?>.</div>
+										<a href="<?php comments_link(); ?>" class="comments-link">
+											<?php comments_number( 'No Responses Yet. Leave a response?','1 Response.', '% Responses.', 'comments-link', 'Sorry, Comments are closed.'); ?></a>
+											<?php the_category(', ') ?>.
+										<span class="tags"><?php the_tags('Tags: ', ', ', '. '); ?></span>
+									</footer>
+									<p><?php the_excerpt(); ?></p>
 
-						<h2 class="post_title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-
-						<footer class="byline meta postmetadata">
-							<time class="published" datetime="<?php the_time('l, F jS, Y') ?>"><?php the_time('l, F jS, Y') ?></time>.
-							<div class="author">Written by <?php the_author(); ?>.</div>
-							<a href="<?php comments_link(); ?>" class="comments-link">
-								<?php comments_number( 'No Responses Yet. Leave a response?','1 Response.', '% Responses.', 'comments-link', 'Sorry, Comments are closed.'); ?></a>
-								<?php the_category(', ') ?>.
-							<span class="tags"><?php the_tags('Tags: ', ', ', '. '); ?></span>
-						</footer>
-						<p><?php the_excerpt(); ?></p>
-
-						</div>
-
-
-
+									</div>
 
 
 
 
+								<?php } // end while
+							} // end if
+						?>
 
+					</div><!-- #main_wrapper -->
+		</div>
 
-
-
-
-					<?php } // end while
-				} // end if
-			?>
-
-		</div><!-- #main_wrapper -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
