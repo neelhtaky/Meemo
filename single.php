f0766ba5edee387960e75dedb05831621e457f9c
neelhtaky <Kat@kathleens-mbp.lan>
2014-05-17 11:58:42 +0300

    Fully Functional Posts Navigation*
    *Note: not styled yet.

--- single.php
+++ single.php
@@ -3,65 +3,53 @@
 <!-- MAIN CONTENT WRAPPER -->
 <section id="entries wrap" class="small-12 medium-8  large-6 xlarge-7  xxlarge-7 columns ">
 
-	<!--<?php next_post_link('<span>%link</span>'); ?> -->
-
-
-
-	<div class="cn-bar">
-		<div class="cn-nav row">
-
-			<?php $prevPost = get_previous_post(true);
-			if($prevPost) {?>
-				<?php $prevthumbnail = get_the_post_thumbnail($prevPost->ID );
-					$post_id_p= $prevPost->ID;
-
-				$post_thumb_id= get_post_thumbnail_id( $post_id_p );
-				$image_attributes = wp_get_attachment_image_src( $post_thumb_id );
-			}?>
-			<?php
-			if ($post_id_p){
-			$prev_post_url = get_permalink($prevPost->ID); ?>
-
-			<a href="<?php echo $prev_post_url ?>" class="cn-nav-prev ">
-				<span>Previous</span>
-				<p><?php echo get_the_title($post_id_p); ?></p>
-
-				<div style="background-image:url('<?php  echo $image_attributes['0'];?>');"></div>
-			</a>
-			<?php } ?>
-
-			<?php $nextPost = get_next_post(true);
-			if($nextPost) { ?>
-			<?php $nextthumbnail = get_the_post_thumbnail($nextPost->ID );
-			$post_id_x= $nextPost->ID;
-
-				$post_thumb_id= get_post_thumbnail_id( $post_id_x );
-				$image_attributes = wp_get_attachment_image_src( $post_thumb_id );
-			} ?>
-	<?php
-	if ($post_id_x){
-	$next_post_url = get_permalink($post_id_x); ?>
-		<a href="<?php echo $next_post_url ?>" class="cn-nav-next">
-			<span>Next</span>
-			<p><?php
-			if ($post_id_x){
-			 echo get_the_title($post_id_x);
-			 } ?></p>
-
-			<div style="background-image:url('<?php  echo $image_attributes['0'];?>');"></div>
-
-		</a>
-	<?php } ?>
-</div>
-</div>
-
 
 <?php get_template_part( 'loop', 'single' ); ?>
+<div class="cn-bar">
+	<div class="cn-nav row">
 
 
 
 
-	<?php get_template_part( 'loop', 'related' ); ?>
+
+<?php
+function generateNavButton($the_post,$className){
+	 if($the_post) {
+		/* get the primary key id from the_post */
+		$post_id_prev= $the_post->ID;
+		/* get thumbnail id from post ID */
+		$post_thumb_id= get_post_thumbnail_id( $post_id_prev );
+		/* get the thumbnail url from thumbnail ID */
+		 $thumb_url = wp_get_attachment_image_src( $post_thumb_id );
+		 /* get the posts url from post ID */
+		 $prev_post_url = get_permalink($the_post->ID);
+
+		/* Display */ ?>
+		<a href="<?php echo $prev_post_url ?>" class="<?php echo $className ?>">
+			<!-- Fallback text is not displayed by css -->
+			<span>Previous</span>
+			<!-- display title -->
+			<p><?php echo get_the_title($post_id_prev); ?></p>
+			<!-- display background image on css hover -->
+			<div style="background-image:url('<?php  echo $thumb_url['0'];?>');"></div>
+		</a>
+
+		<?php } } ?>
+
+	<!-- check if previous post exsists -->
+	<?php
+	$prv_post = get_previous_post(true);
+	generateNavButton($prv_post,"cn-nav-prev");
+	$nxt_post = get_next_post(true);
+	generateNavButton($nxt_post,"cn-nav-next");
+	?>
+
+
+
+
+	</div><!-- .cn-nav .row -->
+</div><!-- cn-bar -->
+<?php get_template_part( 'loop', 'related' ); ?>
 
 
 
