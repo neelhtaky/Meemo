8d893b574a0c093fec06fb9bab165c5d2491f77a
neelhtaky <Kat@kathleens-mbp.lan>
2014-05-18 07:45:02 +0300

    Working Post Nav

--- single.php
+++ single.php
@@ -5,43 +5,48 @@
 
 
 <?php get_template_part( 'loop', 'single' ); ?>
+
+
 <div class="cn-bar">
 	<div class="cn-nav row">
+		<?php
+		function generateNavButton($the_post,$className){
+			 if($the_post) {
+				/* get the primary key id from the_post */
+				$post_id_prev= $the_post->ID;
+				/* get thumbnail id from post ID */
+				$post_thumb_id= get_post_thumbnail_id( $post_id_prev );
+				/* get the thumbnail url from thumbnail ID */
+				 $thumb_url = wp_get_attachment_image_src( $post_thumb_id );
+				 /* get the posts url from post ID */
+				 $prev_post_url = get_permalink($the_post->ID);
 
+		?>
 
+				<a href="<?php echo $prev_post_url ?>" class="<?php echo $className ?> ">
+					<?php
+					if ($className == 'cn-nav-next small-6 columns'){?>
+						<p class="small-8 columns"><?php echo get_the_title($post_id_prev);?> </p>
+					<?php } /* Display */ ?>
 
+					<!-- Fallback text is not displayed by css -->
+					<span class="small-4 columns">Previous</span>
 
-
-<?php
-function generateNavButton($the_post,$className){
-	 if($the_post) {
-		/* get the primary key id from the_post */
-		$post_id_prev= $the_post->ID;
-		/* get thumbnail id from post ID */
-		$post_thumb_id= get_post_thumbnail_id( $post_id_prev );
-		/* get the thumbnail url from thumbnail ID */
-		 $thumb_url = wp_get_attachment_image_src( $post_thumb_id );
-		 /* get the posts url from post ID */
-		 $prev_post_url = get_permalink($the_post->ID);
-
-		/* Display */ ?>
-		<a href="<?php echo $prev_post_url ?>" class="<?php echo $className ?>">
-			<!-- Fallback text is not displayed by css -->
-			<span>Previous</span>
-			<!-- display title -->
-			<p><?php echo get_the_title($post_id_prev); ?></p>
-			<!-- display background image on css hover -->
-			<div style="background-image:url('<?php  echo $thumb_url['0'];?>');"></div>
-		</a>
-
+					<!-- display background image on css hover -->
+					<div class=""style="background-image:url('<?php  echo $thumb_url['0'];?>');"></div>
+					<!-- display title -->
+					<?php
+					if ($className=="cn-nav-prev small-6 columns"){?>
+								<p class="small-8 columns"><?php echo get_the_title($post_id_prev);?> </p>
+					<?php } ?>
+				</a>
 		<?php } } ?>
-
-	<!-- check if previous post exsists -->
+	<!-- check if previous post exists -->
 	<?php
 	$prv_post = get_previous_post(true);
-	generateNavButton($prv_post,"cn-nav-prev");
+	generateNavButton($prv_post,"cn-nav-prev small-6 columns");
 	$nxt_post = get_next_post(true);
-	generateNavButton($nxt_post,"cn-nav-next");
+	generateNavButton($nxt_post,"cn-nav-next small-6 columns");
 	?>
 
 
