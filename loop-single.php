105e9983eb9b69742a697e147eb36b083132e498
neelhtaky <Kat@kathleens-mbp.lan>
2014-05-28 10:41:34 +0300

    Pingback styling finished

--- loop-single.php
+++ loop-single.php
@@ -4,18 +4,23 @@ if (have_posts()) :
 		the_post(); ?>
 
 	<article <?php post_class("clear entry post small-12 medium-12 xlarge-12 xxlarge-12 columns"); ?> id="post-<?php the_ID(); ?>" role="article">
-		<?php if (has_post_thumbnail( )): ?>
 
-	<?php endif ?>
-	<?php if (!is_woocommerce) { ?>
-		<?php if ( is_home() || is_front_page() ) { ?>
-				<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-			<?php } elseif ( is_singular() || is_single() || is_404() || is_archive() ) { ?>
-				<h1><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-			<?php } else { ?>
-				<h1><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-			<?php } ?>
+
+	<?php if (!$woothemes_sensei) { ?>
+		<?php if ( is_singular() || is_single() || is_404() || is_archive() ) { ?>
+						<h1><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+					<?php } else { ?>
+						<h1><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+					<?php } ?>
+	<?php } else { ?>
+	This should show only for woocommerce pages....
 	<?php } ?>
+
+
+
+
+
+
 		<p><?php the_content(); ?></p>
 	</article>
 	<?php endwhile;
--- single.php
+++ single.php
@@ -2,48 +2,7 @@
 <?php get_sidebar('left'); ?>
 <section id="entries wrap" class="small-12 medium-8  large-6 xlarge-7  xxlarge-7 columns ">
 <?php get_template_part( 'loop', 'single' ); ?>
-<div class="cn-bar">
-	<div class="cn-nav row">
-		<?php
-		function generateNavButton($the_post,$className){
-			 if($the_post) {
-				/* get the primary key id from the_post */
-				$post_id_prev= $the_post->ID;
-				/* get thumbnail id from post ID */
-				$post_thumb_id= get_post_thumbnail_id( $post_id_prev );
-				/* get the thumbnail url from thumbnail ID */
-				 $thumb_url = wp_get_attachment_image_src( $post_thumb_id );
-				 /* get the posts url from post ID */
-				 $prev_post_url = get_permalink($the_post->ID);
 
-		?>
-				<a href="<?php echo $prev_post_url ?>" class="<?php echo $className ?> ">
-					<?php
-					if ($className == 'cn-nav-next small-6 columns'){?>
-						<p class="small-8 columns "><?php echo get_the_title($post_id_prev);?> </p>
-					<?php } /* Display */ ?>
-
-					<!-- Fallback text is not displayed by css -->
-					<span class="small-3 columns">Previous</span>
-
-					<!-- display background image on css hover -->
-					<div class=""style="background-image:url('<?php  echo $thumb_url['0'];?>');"></div>
-					<!-- display title -->
-					<?php
-					if ($className=="cn-nav-prev small-6 columns"){?>
-								<p class="small-8 columns push-4"><?php echo get_the_title($post_id_prev);?> </p>
-					<?php } ?>
-				</a>
-		<?php } } ?>
-	<!-- check if previous post exists -->
-	<?php
-	$prv_post = get_previous_post(true);
-	generateNavButton($prv_post,"cn-nav-prev small-6 columns");
-	$nxt_post = get_next_post(true);
-	generateNavButton($nxt_post,"cn-nav-next small-6 columns");
-	?>
-	</div><!-- .cn-nav .row -->
-</div><!-- cn-bar -->
 		<?php if( is_single() || is_page() ){ ?>
 			<?php comments_template(); ?>
 		<?php } ?>
